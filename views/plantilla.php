42a6d31141a11f1404fee8350df0143d653b79ea
Marx Alonso <marxchip99@gmail.com>
2024-03-31 19:02:47 -0500

    Primer commit, subiendo los archivos y las plantilla que se utilizara

---
+++ views/plantilla.php
@@ -0,0 +1,31 @@
+<?php
+    require_once 'views/modulos/header.php';
+		
+	if (isset($_GET['ruta'])) {
+
+
+
+		if ($_GET["ruta"] == "inicio" 
+		||$_GET["ruta"] == "contacto"
+		||$_GET["ruta"] == "nosotros"
+		||$_GET["ruta"] == "servicios"
+		||$_GET["ruta"] == "locales"
+		||$_GET["ruta"] == "terminos-condiciones"
+		//seguros
+
+		 ) {
+	
+			if (file_exists("views/modulos/" . $_GET["ruta"] . ".php")) {
+				include_once "views/modulos/" . $_GET["ruta"] . ".php";
+			}  else if (file_exists("controller/" . $_GET["ruta"] . ".php")) {
+				include_once "controller/" . $_GET["ruta"] . ".php";
+			}
+		} 
+	} else {
+		include_once "views/modulos/inicio.php";
+	}
+		
+	require_once 'views/modulos/footer.php';
+?>
+
+		
