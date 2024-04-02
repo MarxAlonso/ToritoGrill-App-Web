c6978d5f1eeb9ecf67ff3c15fe4428a1f9f5dd52
Marx Alonso <marxchip99@gmail.com>
2024-04-01 22:07:29 -0500

    Mejoras en los apartados de menu y footer

--- views/modulos/footer.php
+++ views/modulos/footer.php
@@ -1,71 +1,57 @@
+<!--O tambien puede solo tener el color rgb(7,18,25)sin transparencia-->
+<footer class="footer-tipo" style="background-color: black;">
+    <div class="container">
+        <div class="row">
+        <div class="footer-col col-sm-6 col-lg-3">
+                <br>
+                <img src="views/img/torito-grill.webp" width="200" alt="Logo empresa">
 
+            </div>
+            <div class="footer-col col-sm-6 col-lg-3">
+                <br>
+                <a class="menus" href="inicio" style="font-size: 16px; color:white; margin-bottom: 12px;">Inicio</a>
+                <a class="menus" href="nosotros" style="font-size: 16px; color:white; margin-bottom: 12px;">Nosotros</a>
+                <a class="menus" href="servicios" style="font-size: 16px; color:white; margin-bottom: 12px;">Servicios</a>
+                <a class="menus" href="/iboutplacement/PreguntasFrecuentes" style="font-size: 16px; color:white; margin-bottom: 12px;">Preguntas Frecuentes</a>
+                <a class="menus" href="https://outplacementt.blogspot.com" style="font-size: 16px; color:white; margin-bottom: 12px;">Blog</a>
+                <a class="menus" href="/iboutplacement/estadisticas" style="font-size: 16px; color:white; margin-bottom: 12px;">Estadísticas</a>
+                <a class="menus" href="https://ibjobcoach.com/" style="font-size: 15px; color:white; margin-bottom: 12px;">IBjobcoach</a>
+            </div>
+            <div class="footer-col col-sm-6 col-lg-3">
+                <br>
+                <span style="font-size: 13px; color:white;"> IBCORP SAC RUC 20601399882</span>
 
-<!--                   Footer                     -->
-<footer id="cs-footer-308" style="margin-top: 100px;">
-    <div class="cs-container">
-        <!-- Logo Group -->
-        <div class="cs-logo-group">
-            <a aria-label="go back to home" class="cs-logo" href="inicio">
-                <img width="160" src="views/img/torito-grill.webp" alt="Logo Empresa">
-            </a>
+                <p style="font-size: 13px; color:white;"> cm.outplacement.coaching@corpibgroup.com</p>
+
+                <p style="font-size: 13px; color:white;"> Central Telefónica: (511) 748 5112</p>
+
+                <p style="font-size: 13px; color:white;">Oficina Central: Av. Circunvalación Club Golf los Incas, Torre 3, Nro 208, Oficina 602B , Urb. Club Golf los Incas , Santiago de Surco, Lima, Perú</p>
+            </div>
+            <div class="footer-col col-sm-6 col-lg-3">
+                <div class="sociales">
+                    <br>
+                    <a class="social-icon" style="background-color: #3c70a4; color: white; margin-bottom: 15px;" href="https://www.facebook.com/IBOutplacementOficial"><i class="bi bi-facebook"></i></a>
+                    <a class="social-icon" style="background-color: #000; color: white; margin-bottom: 15px;" href="https://twitter.com/ib_outplacement"><i class="bi bi-twitter-x"></i></a>
+                    <a class="social-icon" style="background-color: #C8062F; color: white; margin-bottom: 15px;" href="https://www.youtube.com/channel/UC9NkcDwe6-P37cHXkn1XBcA/"><i class="bi bi-youtube"></i></a>
+                    <a class="social-icon" style="background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%); box-shadow: 0px 3px 10px rgba(0,0,0,.25); color: white; margin-bottom: 15px;" href="https://www.instagram.com/iboutplacement/"><i class="bi bi-instagram"></i></a>
+                    <a class="social-icon" style="background-color: #06A0C8; color: white; margin-bottom: 15px;" href="https://www.linkedin.com/company/iboutplacement"><i class="bi bi-tiktok"></i></a>
+                </div>
+            </div>
+
         </div>
-        <!-- Links -->
-        <ul class="cs-nav">
-            <li class="cs-nav-li">
-                <span class="cs-header">Sitemap</span>
-            </li>
-            <li class="cs-nav-li">
-                <a class="cs-nav-link" href="">About</a>
-            </li>
-            <li class="cs-nav-li">
-                <a class="cs-nav-link" href="">Services</a>
-            </li>
-            <li class="cs-nav-li">
-                <a class="cs-nav-link" href="">Portfolio</a>
-            </li>
-            <li class="cs-nav-li">
-                <a class="cs-nav-link" href="">Contact</a>
-            </li>
-        </ul>
-        <ul class="cs-nav">
-            <li class="cs-nav-li">
-                <span class="cs-header">Categories</span>
-            </li>
-            <li class="cs-nav-li">
-                <a class="cs-nav-link" href="">Business Law</a>
-            </li>
-            <li class="cs-nav-li">
-                <a class="cs-nav-link" href="">Education Law</a>
-            </li>
-            <li class="cs-nav-li">
-                <a class="cs-nav-link" href="">Insurance Defense</a>
-            </li>
-            <li class="cs-nav-li">
-                <a class="cs-nav-link" href="">Family Law</a>
-            </li>
-        </ul>
-        <!-- Contact Info -->
-        <ul class="cs-nav">
-            <li class="cs-nav-li">
-                <span class="cs-header">Contact</span>
-            </li>
-            <li class="cs-nav-li">
-                <img class="cs-icon" aria-hidden="true" loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/Contact-Page/phone-stroke.svg" alt="phone" width="24" height="24">
-                <a class="cs-nav-link cs-phone" href="tel: 123-456-7890">(123) 456-7890</a>
-            </li>
-            <li class="cs-nav-li">
-                <img class="cs-icon" aria-hidden="true" loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/Contact-Page/email-stroke.svg" alt="email" width="24" height="24">
-                <a class="cs-nav-link cs-email" href="mailto:info@codestitch.com">info@codestitch.com</a>
-            </li>
-            <li class="cs-nav-li">
-                <img class="cs-icon" aria-hidden="true" loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/Contact-Page/pin-stroke.svg" alt="pin" width="24" height="24">
-                <a class="cs-nav-link cs-address" href="" target="_blank" rel="noopener">
-                    123 Avenue Street<br> Oak Harbor, WA 98101
-                </a>
-            </li>
-        </ul>
-    </div>
-    <div class="cs-bottom">
-        <span class="cs-copyright">© Copyright 2024 - <a class="cs-copyright-link" href="">Marx Chipana</a></span>
     </div>
 </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.js"></script>
+<script src="views/js/jquery-3.5.1.min.js"></script>
+<script src="views/js/bootstrap.bundle.js"></script>
+<script src="views/js/bootstrap.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.min.js"></script>
+
+<script>
+    new WOW().init();
+</script>
+</body>
+</html>
--- views/modulos/menu.php
+++ views/modulos/menu.php
@@ -1,28 +1,59 @@
 <style>
-    li{
-        font-size: 14px;
+    .navegaciones a {
+        transition: font-size 0.1s ease-in-out, color 0.1s ease-in-out, font-weight 1s ease-in-out;
     }
-    @media (max-width: 850px) {
-    li {
-        font-size: 12px;
-      }
-  }
+
+    .navegaciones a {
+        margin-bottom: 20px;
+    }
 </style>
-<ul class="nav justify-content-center px-5 bg-black" style="padding-top: 10px; padding-bottom: 10px; border-top: 2px solid white; border-bottom: 2px solid white;">
-    <a class="navbar-brand" href="inicio"><img src="views/img/torito-grill.webp" alt="Logo Empresa"></a>
-    <li class="nav-item">
-        <a class="nav-link texto text-white" aria-current="page" href="inicio">INICIO</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link texto text-white" href="nosotros">NOSOTROS</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link texto text-white" href="servicios">SERVICIOS</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link texto text-white" href="locales">NUESTROS LOCALES</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link texto text-white" href="contacto" style="background-color: #862510;">CONTACTANOS</a>
-    </li>
-</ul>
+<div class="sticky-top container-fluid p-0 m-0">
+    <nav class="navegaciones">
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="bi bi-list" style="background-color: white; border-radius: 10px;"></i>
+        </label>
+        <a href="inicio" class="enlace">
+            <img src="views/img/torito-grill.webp" alt="" class="logo">
+        </a>
+        <ul>
+            <li><a class="active" href="inicio" style="font-size: 13px;">INICIO</a>
+            <li><a href="nosotros" style="font-size: 13px;">NOSOTROS</a></li>
+            <li><a href="servicios" style="font-size: 13px;">SERVICIOS</a></li>
+            <li><a href="https://outplacementt.blogspot.com/" style="font-size: 16px;">BLOG</a></li>
+            <li><a class="contact" href="contacto" style="font-size: 13px; padding: 10px 20px; color: white;">CONTÁCTANOS </a></li>
+            <li><a href="/iboutplacement/estadisticas" style="font-size: 13px;">ESTADÍSTICAS</a></li>
+        </ul>
+    </nav>
+</div>
+<script>
+    var navLinks = document.querySelectorAll('.navegaciones a');
+
+    navLinks.forEach(function(link) {
+        link.addEventListener('mouseover', function() {
+            link.style.fontSize = '15px';
+            link.style.color = 'red';
+            link.style.fontWeight = 'bold';
+        });
+
+        link.addEventListener('mouseout', function() {
+            link.style.fontSize = '13px';
+            link.style.color = link.classList.contains('contact') ? 'white' : 'white';
+            link.style.fontWeight = 'normal';
+        });
+    });
+</script>
+<script>
+    var logoLink = document.querySelector('.navegaciones .enlace');
+    var logo = document.querySelector('.navegaciones .logo');
+
+    logo.style.transition = 'transform 0.3s ease'; // Esto agrega transición suave en el logo de la imagen
+
+    logoLink.addEventListener('mouseover', function() {
+        logo.style.transform = 'scale(1.2)'; // Esto aumenta el tamaño del logo al pasar el mouse sobre el enlace
+    });
+
+    logoLink.addEventListener('mouseout', function() {
+        logo.style.transform = 'scale(1)'; // En esta parte regresa el logo a su tamaño original al quitar el mouse del enlace
+    });
+</script>
