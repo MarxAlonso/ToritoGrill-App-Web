443f37e9514f56d730863097148e6f2b70007711
Marx Alonso <marxchip99@gmail.com>
2024-04-03 14:36:23 -0500

    Modificaciones en menu y servicios

--- views/modulos/menu.php
+++ views/modulos/menu.php
@@ -1,12 +1,3 @@
-<style>
-    .navegaciones a {
-        transition: font-size 0.1s ease-in-out, color 0.1s ease-in-out, font-weight 1s ease-in-out;
-    }
-
-    .navegaciones a {
-        margin-bottom: 20px;
-    }
-</style>
 <div class="sticky-top container-fluid p-0 m-0">
     <nav class="navegaciones">
         <input type="checkbox" id="check">
@@ -17,32 +8,16 @@
             <img src="views/img/torito-grill.webp" alt="" class="logo">
         </a>
         <ul>
-            <li><a class="active" href="inicio" style="font-size: 13px;">INICIO</a>
-            <li><a href="nosotros" style="font-size: 13px;">NOSOTROS</a></li>
-            <li><a href="servicios" style="font-size: 13px;">SERVICIOS</a></li>
-            <li><a href="https://outplacementt.blogspot.com/" style="font-size: 13px;">BLOG</a></li>
-            <li><a href="/iboutplacement/estadisticas" style="font-size: 13px;">ESTADÍSTICAS</a></li>
-            <li><a class="contact" href="contacto" style="font-size: 13px; padding: 10px 20px; color: white;">CONTÁCTANOS </a></li>
+            <li><a class="active" href="inicio" style="font-size: 13px;"><b>INICIO</b></a>
+            <li><a href="nosotros" style="font-size: 13px;"><b>NOSOTROS</b></a></li>
+            <li><a href="servicios" style="font-size: 13px;"><b>SERVICIOS</b></a></li>
+            <li><a href="https://outplacementt.blogspot.com/" style="font-size: 13px;"><b>BLOG</b></a></li>
+            <li><a href="/iboutplacement/estadisticas" style="font-size: 13px;"><b>ESTADÍSTICAS</b></a></li>
+            <li><a class="contact" href="contacto" style="font-size: 13px; padding: 10px 20px; color: white;"><b>CONTÁCTANOS</b></a></li>
         </ul>
     </nav>
 </div>
-<script>
-    var navLinks = document.querySelectorAll('.navegaciones a');
 
-    navLinks.forEach(function(link) {
-        link.addEventListener('mouseover', function() {
-            link.style.fontSize = '15px';
-            link.style.color = '#862510';
-            link.style.fontWeight = 'bold';
-        });
-
-        link.addEventListener('mouseout', function() {
-            link.style.fontSize = '13px';
-            link.style.color = link.classList.contains('contact') ? 'white' : 'white';
-            link.style.fontWeight = 'normal';
-        });
-    });
-</script>
 <script>
     var logoLink = document.querySelector('.navegaciones .enlace');
     var logo = document.querySelector('.navegaciones .logo');
--- views/modulos/servicios.php
+++ views/modulos/servicios.php
@@ -1,18 +1,3 @@
-<style>
-    .consulta{
-        border: 1px solid #862510;
-        background-color: #862510; 
-        border-radius: 20px; 
-        color:white;
-        padding:5px;
-        text-decoration: none;
-    }
-    .consulta:hover{
-        border: 1px solid #862510;
-        background-color: #fff;
-        color: #862510;
-    }
-</style>
 <div class="container-fluid px-5" style="margin-bottom: 30px;">
     <div class="row justify-content-center">
         <div class="col-12 col-md-10 col-lg-8" style="margin-top: 100px;">
@@ -26,13 +11,6 @@
             <p>Música, cocteles, piqueos, comida y más. Todo en un mismo lugar, celebra con nosotros en nuestro Bar. 🍾</p><br>
             <h3 style="margin-top: 30px;"><b>Piso 4:</b></h3><br>
             <p>Una linda terraza te espera, celebra tus eventos en este espacio con una amplia vista, buena música, tus platos y bebidas favoritas también están aquí. ✨</p><br>
-        </div>
-    </div>
-</div>
-<div class="container px-5">
-    <div class="row justify-content-center">
-        <div class="col-12 text-center">
-            <h2 style="background-color: #862510; color: white;">¡Conoce a nuestro equipo!</h2>
         </div>
     </div>
 </div>
