22e7e15d2269a1f2d0a2d2d16b89f014aa4af30d
Marx Alonso <marxchip99@gmail.com>
2024-04-03 12:43:10 -0500

    Realizacion del apartado de servicios

---
+++ views/modulos/servicios.php
@@ -0,0 +1,39 @@
+<style>
+    .consulta{
+        border: 1px solid #862510;
+        background-color: #862510; 
+        border-radius: 20px; 
+        color:white;
+        padding:5px;
+        text-decoration: none;
+    }
+    .consulta:hover{
+        border: 1px solid #862510;
+        background-color: #fff;
+        color: #862510;
+    }
+</style>
+<div class="container-fluid px-5" style="margin-bottom: 30px;">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-8" style="margin-top: 100px;">
+            <h3><b>¡Reserva con nosotros!</b></h3><br>
+            <p>Celebra reuniones corporativas, celebraciones familiares, celebracion de cumpleaños, aniversarios, etc. Aceptamos todo tipo de reservas.</p><br>
+            <a class="consulta" href="contacto">Consulta Aqui</a>
+
+            <h3 style="margin-top: 30px;"><b>Piso 2:</b></h3><br>
+            <p>¡Celebra con nosotros! <br> Una reunión en familia, una reunión laboral, o lo que gustes. El espacio es amplio y agradable para tu estadía.</p><br>
+            <h3 style="margin-top: 30px;"><b>Piso 3:</b></h3><br>
+            <p>Música, cocteles, piqueos, comida y más. Todo en un mismo lugar, celebra con nosotros en nuestro Bar. 🍾</p><br>
+            <h3 style="margin-top: 30px;"><b>Piso 4:</b></h3><br>
+            <p>Una linda terraza te espera, celebra tus eventos en este espacio con una amplia vista, buena música, tus platos y bebidas favoritas también están aquí. ✨</p><br>
+        </div>
+    </div>
+</div>
+<div class="container px-5">
+    <div class="row justify-content-center">
+        <div class="col-12 text-center">
+            <h2 style="background-color: #862510; color: white;">¡Conoce a nuestro equipo!</h2>
+        </div>
+    </div>
+</div>
+
