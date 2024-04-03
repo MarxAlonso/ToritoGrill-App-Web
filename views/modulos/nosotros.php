bad6608e60bec0e6bba314497b87035e0f027fac
Marx Alonso <marxchip99@gmail.com>
2024-04-03 12:01:53 -0500

    Apartado de nosotros

---
+++ views/modulos/nosotros.php
@@ -0,0 +1,27 @@
+<div class="container-fluid px-5" style="margin-bottom: 30px;">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-8" style="margin-top: 100px;">
+            <h3><b>¿Quiénes somos?</b></h3><br>
+            <h3 style="margin-top: 30px;"><b>Visión:</b></h3><br>
+            <p>Queremos ser el restaurante a primera elección en Los Olivos, y en todas partes, donde la comida deliciosa y un servicio excepcional se combinan para crear momentos felices.</p><br>
+            <h3 style="margin-top: 30px;"><b>Misión:</b></h3><br>
+            <p>En Torito Grill, nuestra misión es simple: servir la mejor comida, las bebidas más deliciosas y brindar la mejor atención. Queremos que cada visita sea especial, que disfrutes de tus platos favoritos, bebidas de alta calidad y que te sientas como en casa. Nuestro equipo está dedicado a hacer que tu estadía sea inolvidable, porque tu satisfacción es nuestra prioridad número uno. Buscamos mejorar constantemente para ofrecer la mejor experiencia culinaria.</p><br>
+            
+            <h3 style="margin-top: 30px;"><b>Nuestros Valores:</b></h3><br>
+            <ol>
+                <li><b>Calidad:</b> Utilizamos los insumos más frescos y de la más alta calidad garantizando así que su estadía sea de total agrado</li>
+                <li><b>Pasión:</b> La pasión por los sabores auténticos impulsa cada plato que servimos. Nuestro equipo está comprometido en brindar una atención cálida y personalizada.</li>
+                <li><b>Sostenibilidad:</b> Trabajamos con agricultores y proveedores de la región, promoviendo prácticas sostenibles y responsabilidad social.</li>
+                <li><b>Encuentro:</b> Torito Grill es el lugar ideal para compartir momentos especiales a la hora de comer y brindar ya sea en familia, con amigos o en pareja.</li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="container px-5">
+    <div class="row justify-content-center">
+        <div class="col-12 text-center">
+            <h2 style="background-color: #862510; color: white;">¡Conoce a nuestro equipo!</h2>
+        </div>
+    </div>
+</div>
+
