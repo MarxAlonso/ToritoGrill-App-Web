2e30a9a9c1c4381712cd3e77f36a0b05714729d2
Marx Alonso <marxchip99@gmail.com>
2024-04-04 17:10:50 -0500

    Mejorando un poco los estilos

--- views/modulos/servicios.php
+++ views/modulos/servicios.php
@@ -1,33 +1,33 @@
 <div class="container-fluid px-5" style="margin-bottom: 30px;">
     <div class="row justify-content-center">
-        <div class="col-12 col-md-10 col-lg-8" style="margin-top: 100px;">
+        <div class="col-12 col-md-10 col-lg-8" style="margin-top: 100px; margin-bottom: 100px;">
             <h3><b>¡Reserva con nosotros!</b></h3><br>
             <p>Celebra reuniones corporativas, celebraciones familiares, celebracion de cumpleaños, aniversarios, etc. Aceptamos todo tipo de reservas.</p><br>
             <a class="consulta" href="contacto">Consulta Aqui</a>
-
-            <div class="col-md-3">
-                <img src="views/img/piso2.webp" alt="Piso 2">
-            </div>
-            <div class="col-md-3">
-                <h3 style="margin-top: 30px;"><b>Piso 2:</b></h3><br>
-                <p>¡Celebra con nosotros! <br> Una reunión en familia, una reunión laboral, o lo que gustes. El espacio es amplio y agradable para tu estadía.</p><br>
-            </div>
-
-            <div>
-                <h3 style="margin-top: 30px;"><b>Piso 3:</b></h3><br>
-                <p>Música, cocteles, piqueos, comida y más. Todo en un mismo lugar, celebra con nosotros en nuestro Bar. 🍾</p><br>
-            </div>
-            <div>
-                <img src="" alt="">
-            </div>
-
-            <div>
-                <img src="" alt="">
-            </div>
-            <div>
-                <h3 style="margin-top: 30px;"><b>Piso 4:</b></h3><br>
-                <p>Una linda terraza te espera, celebra tus eventos en este espacio con una amplia vista, buena música, tus platos y bebidas favoritas también están aquí. ✨</p><br>
-            </div>
+            <br>
+        </div>
+        <div class="col-md-6">
+            <img src="views/img/piso2.webp" alt="Piso 2">
+        </div>
+        <div class="col-md-6">
+            <h3 style="margin-top: 30px;"><b>Piso 2:</b></h3><br>
+            <p>¡Celebra con nosotros! <br> Una reunión en familia, una reunión laboral, o lo que gustes. El espacio es amplio y agradable para tu estadía.</p><br>
+        </div>
+        
+        <div class="col-md-6">
+            <h3 style="margin-top: 30px;"><b>Piso 3:</b></h3><br>
+            <p>Música, cocteles, piqueos, comida y más. Todo en un mismo lugar, celebra con nosotros en nuestro Bar. 🍾</p><br>
+        </div>
+        <div class="col-md-6">
+            <img src="views/img/piso3.webp" alt="">
+        </div>
+        
+        <div class="col-md-6">
+            <img src="views/img/piso4.webp" alt="">
+        </div>
+        <div class="col-md-6">
+            <h3 style="margin-top: 30px;"><b>Piso 4:</b></h3><br>
+            <p>Una linda terraza te espera, celebra tus eventos en este espacio con una amplia vista, buena música, tus platos y bebidas favoritas también están aquí. ✨</p><br>
         </div>
     </div>
 </div>
