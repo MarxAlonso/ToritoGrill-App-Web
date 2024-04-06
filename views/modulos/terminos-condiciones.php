8ea752318621bb83c4271184bf685c650302c0cf
Marx Alonso <marxchip99@gmail.com>
2024-04-06 17:22:26 -0500

    Modificacion en terminos y condiciones

--- views/modulos/terminos-condiciones.php
+++ views/modulos/terminos-condiciones.php
@@ -1,4 +1,4 @@
-<div class="container-fluid p-5" style="margin-bottom: 60px;">
+<div class="container-fluid p-5" style="margin-bottom: 40px;">
     <div class="row justify-content-center">
         <div class="col-12 col-md-10 col-lg-8 my-auto">
             <h3><b>Conoce nuestros términos y condiciones</b></h3><br>
@@ -7,7 +7,19 @@
             <p style="margin-top: 30px; font-size: 22px;"><b>Promoción de cumpleaños:</b> En Torito Grill celebramos tu cumpleaños de una manera especial. Si vienes a celebrar tu cumpleaños con nosotros, puedes disfrutar de nuestros premios exclusivos. El cumpleañero deberá identificarse con su DNI y tendrá que elegir una de las tres opciones.</p>
             
             <p style="margin-top: 30px; font-size: 22px;"><b>Reservas:</b> Se aceptan todo tipo de reservas, consultar al +51 978 105 066 para ver la disponibilidad de fechas. Toda reserva es con el 50% de adelanto. Atención de comidas hasta las 11:00pm y bar hasta las 12:00pm.</p><br>
-            <p style="font-size: 18px;">Obs: No permitimos el ingreso de bebidas o comidas externas. Así como también personas en estado etílico.</p>
+            <p style="font-size: 18px;"><i>Obs: No permitimos el ingreso de bebidas o comidas externas. Así como también personas en estado etílico.</i></p>
         </div>
     </div>
 </div>
+<div class="container">
+    <hr style="border: 1px solid #000; margin-bottom: 25px;">
+    <div class="text-center redes">
+        <h2><b>Visita nuestras redes sociales:</b></h2><br>
+        <a href="https://www.facebook.com/people/Torito-Grill/100063782141554/" target="_blank"><i class="bi bi-facebook"></i> Facebook</a>
+        <a href="https://www.instagram.com/toritogrillperu/" target="_blank"><i class="bi bi-instagram"></i> Instagram</a>
+        <a href="https://www.tiktok.com/@toritogrillperu" target="_blank"><i class="bi bi-tiktok"></i> Tik Tok</a>
+        <a href="https://wa.link/3mxkpp" target="_blank"><i class="bi bi-whatsapp"></i> Whatsapp</a>
+    </div>
+    <hr style="border: 1px solid #000;  margin-bottom: 25px;">
+</div>
+<br>
