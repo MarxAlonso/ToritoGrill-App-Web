3a06198e302ddeded69d18daba18a198193cdc62
Marx Alonso <marxchip99@gmail.com>
2024-04-06 16:16:51 -0500

    mejorando rutas del footer pt1

--- views/modulos/footer.php
+++ views/modulos/footer.php
@@ -14,9 +14,9 @@
                 <a class="menus" href="inicio" style="font-size: 16px; color:white; margin-bottom: 12px;">Inicio</a>
                 <a class="menus" href="nosotros" style="font-size: 16px; color:white; margin-bottom: 12px;">Nosotros</a>
                 <a class="menus" href="servicios" style="font-size: 16px; color:white; margin-bottom: 12px;">Servicios</a>
-                <a class="menus" href="/iboutplacement/PreguntasFrecuentes" style="font-size: 16px; color:white; margin-bottom: 12px;">Preguntas Frecuentes</a>
+                <a class="menus" href="locales" style="font-size: 16px; color:white; margin-bottom: 12px;">Nuestro Locales</a>
                 <a class="menus" href="https://outplacementt.blogspot.com" style="font-size: 16px; color:white; margin-bottom: 12px;">Blog</a>
-                <a class="menus" href="/iboutplacement/estadisticas" style="font-size: 16px; color:white; margin-bottom: 12px;">Estadísticas</a>
+                <a class="menus" href="contacto" style="font-size: 16px; color:white; margin-bottom: 12px;">Contacto</a>
             </div>
             <div class="footer-col col-sm-6 col-lg-3">
                 <br>
