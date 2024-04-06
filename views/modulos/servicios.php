c575dacded982082af203399c5cff7a34da16f7c
Marx Alonso <marxchip99@gmail.com>
2024-04-06 16:15:03 -0500

    Finalizacion del apartado de servicios

--- views/modulos/servicios.php
+++ views/modulos/servicios.php
@@ -10,41 +10,76 @@
 </div>
 
 
-<div class="container-fluid" style="margin-bottom: -15px;">
+<div class="container-fluid" style="margin-bottom: -15px; background-color: #FFF3E4;">
     <div class="row justify-content-center">
-        <div class="col-md-4 py-md-5">
+        <div class="col-md-4 py-md-5" style="margin-top: 10px;">
             <img src="views/img/piso2.webp" alt="Piso 2" class="img-fluid">
         </div>
-        <div class="col-md-4 py-md-5" >
+        <div class="col-md-4 py-md-5">
             <h3 style="margin-top: 30px;"><b>Piso 2:</b></h3><br>
             <p>¡Celebra con nosotros!🥳 <br> Una reunión en familia, una reunión laboral, o lo que gustes. El espacio es amplio y agradable para tu estadía.</p><br>
         </div>
     </div>
 </div>
 
-<div class="container-fluid p-5" style="margin-bottom: -15px;">
+<div class="container-fluid p-5" style="margin-bottom: -15px; background-color: bisque;">
     <div class="row justify-content-center">
-        <div class="col-md-4 py-md-5" >
+        <div class="col-md-4 py-md-5">
             <h3 style="margin-top: 30px;"><b>Piso 3:</b></h3><br>
             <p>Música, cocteles, piqueos, comida y más. Todo en un mismo lugar, celebra con nosotros en nuestro Bar. 🍾</p><br>
         </div>
-        <div class="col-md-4 py-md-5" >
+        <div class="col-md-4 py-md-5">
             <img src="views/img/piso3.webp" alt="Piso 3" class="img-fluid">
         </div>
     </div>
 </div>
 
-<div class="container-fluid p-5" style="margin-bottom: -15px;">
+<div class="container-fluid p-5" style="margin-bottom: -15px; background-color: #FFF3E4;">
     <div class="row justify-content-center">
-        <div class="col-md-4 py-md-5" >
+        <div class="col-md-4 py-md-5">
             <img src="views/img/piso4.webp" alt="Piso 4" class="img-fluid">
         </div>
-        <div class="col-md-4 py-md-5" >
+        <div class="col-md-4 py-md-5">
             <h3 style="margin-top: 30px;"><b>Piso 4:</b></h3><br>
             <p>Una linda terraza te espera, celebra tus eventos en este espacio con una amplia vista, buena música, tus platos y bebidas favoritas también están aquí. ✨</p><br>
         </div>
     </div>
 </div>
+<br><br>
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-8"> <!-- Ajusta el tamaño de la columna según tus necesidades -->
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-12 mb-3">
+                                <img src="views/img/sala1.webp" alt="Piso 2" class="img-fluid">
+                            </div>
+                            <div class="col-12 mb-3">
+                                <img src="views/img/sala2.webp" alt="Piso 4" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="views/img/sala3.webp" alt="Piso 3" class="img-fluid">
+                    </div>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-8 text-center"> <!-- Ajusta el tamaño de la columna según tus necesidades -->
+            <a href="contacto" class="reserva">RESERVA AQUÍ</a>
+        </div>
+    </div>
+</div>
+
+
 
 <br>
 <div class="container">
