99666ffafbdbf48ccb0a1e541e42b5e010a15ee3
Marx Alonso <marxchip99@gmail.com>
2024-04-06 17:10:41 -0500

    Ulltimos  avances por completo del proyecto - creacion del apartado de Carta, modificacion de ello en los estilos, agregacion de imagenes

---
+++ views/modulos/carta.php
@@ -0,0 +1,58 @@
+<div class="container-fluid p-5" style="margin-bottom: -15px;">
+    <div class="row justify-content-center" style="text-align: center;">
+        <div class="col-md-8 text-center"> <!-- Ajusta el tamaño de la columna según tus necesidades -->
+            <p style="background-color: #862510; color: white; padding-top: 10px; padding-bottom: 10px;">Disfruta de nuestros ricos platos en salón o pídelo por delivery.</p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid p-5" style="margin-bottom: 15px;">
+    <div class="row justify-content-center">
+        <div class="col-md-8 text-center">
+            <img src="views/img/carta1.webp" alt="Carta 1" class="img-fluid">
+        </div>
+    </div>
+    <br>
+    <!-- Nueva fila para las dos imágenes -->
+    <div class="row justify-content-center">
+        <div class="col-md-4 text-center">
+            <img src="views/img/carta2.webp" alt="Carta 2" class="img-fluid">
+        </div>
+        <div class="col-md-4 text-center">
+            <img src="views/img/carta3.webp" alt="Carta 3" class="img-fluid">
+        </div>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-8 text-center">
+            <img src="views/img/carta4.webp" alt="Carta 4" class="img-fluid">
+        </div>
+    </div><br>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center"> <!-- Ajusta el tamaño de la columna según tus necesidades -->
+                <a href="https://toritogrillpe.com/wp-content/uploads/2023/10/carta.pdf" target="_blank" class="nuestra-carta">DESCARGA NUESTRA CARTA</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<br>
+<div class="container">
+    <hr style="border: 2px solid #000; margin-bottom: 30px;">
+    <div class="text-center redes">
+        <p style="font-size: 20px;">Disfruta de tus platos favoritos en pollos, carnes y parrillas. Además, deléitate con nuestros cocteles de la casa y piqueos del Bar.</p><br>
+    </div>
+</div>
+<div class="container">
+    <hr style="border: 2px solid #000; margin-bottom: 15px;">
+    <div class="text-center redes">
+        <h2><b>Visita nuestras redes sociales:</b></h2><br>
+        <a href="https://www.facebook.com/people/Torito-Grill/100063782141554/" target="_blank"><i class="bi bi-facebook"></i> Facebook</a>
+        <a href="https://www.instagram.com/toritogrillperu/" target="_blank"><i class="bi bi-instagram"></i> Instagram</a>
+        <a href="https://www.tiktok.com/@toritogrillperu" target="_blank"><i class="bi bi-tiktok"></i> Tik Tok</a>
+        <a href="https://wa.link/3mxkpp" target="_blank"><i class="bi bi-whatsapp"></i> Whatsapp</a>
+    </div>
+</div>
+<br>
