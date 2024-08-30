57f717d93064822073f5a28ea054569d734fbacd
Marx Alonso <marxchip99@gmail.com>
2024-08-29 23:15:18 -0500

    mejora de contratos

--- views/client/contratos.php
+++ views/client/contratos.php
@@ -1,79 +1,62 @@
 <!-- ============================================ -->
-<!--                    Team                      -->
+<!--                    Contratos                -->
 <!-- ============================================ -->
 
 <section class="container" id="meet-team-2071">
     <div class="cs-container">
         <div class="cs-content">
-            <span class="cs-topper">Our Team</span>
-            <h2 class="cs-title">Meet Our Team</h2>
+            <span class="cs-topper">Nuestros Servicios</span>
+            <h2 class="cs-title">Contrata Nuestros Servicios</h2>
             <p class="cs-text">
-                Orci eu lobortis elementum nibh tellus. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Luctus accumsan tortor posuere ac ut consequat semper. Et pharetra pharetra massa massa ultricies mi quis.
+                En ToritoGrill, ofrecemos una amplia gama de servicios para hacer de tu evento una experiencia inolvidable. Ya sea que estés planeando una reunión, fiesta, boda, o cualquier tipo de celebración, contamos con opciones flexibles y de la más alta calidad para satisfacer tus necesidades.
             </p>
         </div>
         <ul class="cs-card-group">
             <li class="cs-item">
-                <span class="cs-name">Dr. Ronald Richards</span>
+                <span class="cs-name">Servicio de Envío de Platos</span>
                 <div class="cs-flex">
-                    <span class="cs-job">Dental Surgeon</span>
-                    <span class="cs-department">Cosmetic and Aesthetic Dentistry</span>
+                    <span class="cs-job">Entrega a Domicilio</span>
+                    <span class="cs-department">Reuniones y Eventos</span>
                 </div>
+                <p class="cs-item-text">
+                    Selecciona tus platillos favoritos y la cantidad que necesites para tu evento. Nos encargamos de llevar la mejor comida hasta la puerta de tu celebración, garantizando frescura y sabor.
+                </p>
             </li>
             <li class="cs-item">
-                <span class="cs-name">Dr. Cameron Williamson</span>
+                <span class="cs-name">Contratación de Chefs</span>
                 <div class="cs-flex">
-                    <span class="cs-job">Dental Surgeon</span>
-                    <span class="cs-department">Cosmetic and Aesthetic Dentistry</span>
+                    <span class="cs-job">Cocineros Profesionales</span>
+                    <span class="cs-department">Fiestas y Reuniones</span>
                 </div>
+                <p class="cs-item-text">
+                    Disfruta de la presencia de nuestros talentosos chefs en tu evento. Ya sea para una fiesta privada, quinceañero, cumpleaños, o cualquier otra ocasión especial, nuestros chefs harán que tu comida sea inolvidable.
+                </p>
             </li>
             <li class="cs-item">
-                <span class="cs-name">Dr. Brooklyn Simmons</span>
+                <span class="cs-name">Servicio de Bartender</span>
                 <div class="cs-flex">
-                    <span class="cs-job">Dental Surgeon</span>
-                    <span class="cs-department">Cosmetic and Aesthetic Dentistry</span>
+                    <span class="cs-job">Bartenders Profesionales</span>
+                    <span class="cs-department">Preparación de Cócteles</span>
                 </div>
+                <p class="cs-item-text">
+                    Contrata a nuestros bartenders para que preparen los mejores cócteles en tu fiesta. Hacemos que cada bebida sea una obra de arte que tus invitados disfrutarán.
+                </p>
             </li>
             <li class="cs-item">
-                <span class="cs-name">Dr. Savannah Nguyen</span>
+                <span class="cs-name">Alquiler de Salas</span>
                 <div class="cs-flex">
-                    <span class="cs-job">Dental Surgeon</span>
-                    <span class="cs-department">Cosmetic and Aesthetic Dentistry</span>
+                    <span class="cs-job">Salas de Eventos</span>
+                    <span class="cs-department">Buffet Incluido</span>
                 </div>
-            </li>
-            <li class="cs-item">
-                <span class="cs-name">Dr. Albert Flores</span>
-                <div class="cs-flex">
-                    <span class="cs-job">Dental Surgeon</span>
-                    <span class="cs-department">Cosmetic and Aesthetic Dentistry</span>
-                </div>
-            </li>
-            <li class="cs-item">
-                <span class="cs-name">Dr. Eleanor Pena</span>
-                <div class="cs-flex">
-                    <span class="cs-job">Dental Surgeon</span>
-                    <span class="cs-department">Cosmetic and Aesthetic Dentistry</span>
-                </div>
-            </li>
-            <li class="cs-item">
-                <span class="cs-name">Dr. Dianne Russell</span>
-                <div class="cs-flex">
-                    <span class="cs-job">Dental Surgeon</span>
-                    <span class="cs-department">Cosmetic and Aesthetic Dentistry</span>
-                </div>
-            </li>
-            <li class="cs-item">
-                <span class="cs-name">Dr. Darlene Robertson</span>
-                <div class="cs-flex">
-                    <span class="cs-job">Dental Surgeon</span>
-                    <span class="cs-department">Cosmetic and Aesthetic Dentistry</span>
-                </div>
+                <p class="cs-item-text">
+                    Alquila nuestras exclusivas salas de cada local para tu evento especial. Incluimos un servicio de buffet personalizado según el acuerdo entre ambas partes, asegurando que cada detalle de tu evento esté cubierto.
+                </p>
             </li>
         </ul>
     </div>
     <picture class="cs-background">
         <source media="(max-width: 600px)" srcset="views/img/nosotros1.webp">
         <source media="(min-width: 601px)" srcset="views/img/nosotros1.webp">
-        <img loading="lazy" decoding="async" src="views/img/nosotros1.webp" alt="dentist staff" width="1024" height="894">
+        <img loading="lazy" decoding="async" src="views/img/nosotros1.webp" alt="staff ToritoGrill" width="1024" height="894">
     </picture>
 </section>
-                                
